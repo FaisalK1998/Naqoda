995f76b65cdffc0bdfcf9fa8f3b39aefadd4b463
Faisal Khan <faisalalam98@gmail.com>
2021-09-03 16:19:45 +0100

    Started implementing the Dijkstra algorithm

---
+++ pathfinder/src/algorithms/dijkstra.js
@@ -0,0 +1,51 @@
+/**
+ * Dijkstra’s Algorithm lets us prioritise which paths to explore. Instead of exploring all possible paths equally, 
+ * it favors lower cost paths. It is the algorithm of choice for finding the 
+ * shortest paths with multiple destinations
+ */
+
+/**
+ * This function takes in 3 parameters and calculates the shortest path using 
+ * the Dijkstra algorithm
+ * @param {*} grid 
+ * @param {*} startNode 
+ * @param {*} finishNode 
+ * @returns 
+ */
+export function dijkstra(grid, startNode, finishNode) {
+    const visitedNodesInOrder = [];
+    startNode.distance = 0;
+    const unvisitedNodes = getAllNodes(grid);
+  
+        while (unvisitedNodes.length) {
+        sortNodesByDistance(unvisitedNodes);
+        const closestNode = unvisitedNodes.shift();
+        // If we encounter a wall, we skip it.
+            if (!closestNode.isWall) {
+                // If the closest node is at a distance of infinity,
+                // we must be trapped and should stop.
+                if (closestNode.distance === Infinity) return visitedNodesInOrder;
+                closestNode.isVisited = true;
+                visitedNodesInOrder.push(closestNode);
+                if (closestNode === finishNode) return visitedNodesInOrder;
+                updateUnvisitedNeighbors(closestNode, grid);
+            }
+        }
+  }
+  
+  function getAllNodes(grid) {
+
+  }
+  
+  function sortNodesByDistance(unvisitedNodes) {
+
+  }
+  
+  function updateUnvisitedNeighbors(node, grid) {
+
+  }
+  
+  function getUnvisitedNeighbors(node, grid) {
+
+  }
+  
