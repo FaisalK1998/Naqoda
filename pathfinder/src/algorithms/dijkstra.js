87a45dba789b40017f4c126312b56a54e838f525
Faisal Khan <faisalalam98@gmail.com>
2021-09-03 16:29:40 +0100

    Completed Dijkstra's algorithm with the final 2 functions

--- pathfinder/src/algorithms/dijkstra.js
+++ pathfinder/src/algorithms/dijkstra.js
@@ -58,11 +58,34 @@ export function dijkstra(grid, startNode, finishNode) {
         unvisitedNodes.sort((nodeA, nodeB) => nodeA.distance - nodeB.distance);
     }
   
-  function updateUnvisitedNeighbors(node, grid) {
-
-  }
+    /**
+     * This function takes in 2 parameters and updates any unvisited neighbouring nodes
+     * @param {*} node 
+     * @param {*} grid 
+     */
+    function updateUnvisitedNeighbors(node, grid) {
+        const unvisitedNeighbors = getUnvisitedNeighbors(node, grid);
+            //Simple for loop to calculate the distance between neighbours
+            for (const neighbor of unvisitedNeighbors) {
+                neighbor.distance = node.distance + 1;
+                neighbor.previousNode = node;
+            }
+    }
   
-  function getUnvisitedNeighbors(node, grid) {
-
-  }
+    /**
+     * This function retrieves unvisited neighbours
+     * @param {*} node 
+     * @param {*} grid 
+     * @returns The elements of an array that meet the condition specified in the function
+     */
+    function getUnvisitedNeighbors(node, grid) {
+        const neighbors = [];
+        const {col, row} = node;
+            //The push() method appends new elements to the end of an array, and returns the new length of the array.
+            if (row > 0) neighbors.push(grid[row - 1][col]);
+            if (row < grid.length - 1) neighbors.push(grid[row + 1][col]);
+            if (col > 0) neighbors.push(grid[row][col - 1]);
+            if (col < grid[0].length - 1) neighbors.push(grid[row][col + 1]);
+        return neighbors.filter(neighbor => !neighbor.isVisited);
+    }
   
